b0e8d36f3e645e075944a196f2d278cfc5eb2af7
KhaledSaud70 <Khaled.robusta@gmail.com>
2023-11-09 17:06:46 +0300

    Initial commit

--- dist_train.sh
+++ dist_train.sh
@@ -3,7 +3,7 @@
 MODEL=$1
 nGPUs=$2
 
-OUTPUT_DIR=/root/workspace/efficientformer_s0_50d
+OUTPUT_DIR=/root/workspace/scatformer_s0_50d
 
 # python -m torch.distributed.launch --nproc_per_node=$nGPUs --use_env main.py --model $MODEL \
 # --data-path drive/MyDrive/ImageNet100 \
