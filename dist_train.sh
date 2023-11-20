8b4876ad8f26b9fe042902afba3320ce84dca0b3
KhaledSaud70 <Khaled.robusta@gmail.com>
2023-11-20 14:24:18 +0300

    add checkpoints

--- dist_train.sh
+++ dist_train.sh
@@ -3,7 +3,7 @@
 MODEL=$1
 nGPUs=$2
 
-OUTPUT_DIR=/root/workspace/scatformer_s0_300d
+OUTPUT_DIR=/root/workspace/efficientformer_s0_100d
 
 # python -m torch.distributed.launch --nproc_per_node=$nGPUs --use_env /root/workspace/ScatFormer/main.py --model $MODEL \
 # --data-path /root/workspace/ImageNet100 \
@@ -15,8 +15,9 @@ OUTPUT_DIR=/root/workspace/scatformer_s0_300d
 python /root/workspace/ScatFormer/main.py --model $MODEL \
 --data-path /root/workspace/ImageNet100 \
 --output_dir $OUTPUT_DIR \
---batch-size 128 \
---epochs 300 \
+--batch-size 256 \
+--epochs 100 \
+--resume /root/workspace/ScatFormer/efficientformer_s0_100d/checkpoint.pth \
 --distillation-type none \
 
 
