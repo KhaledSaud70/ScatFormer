2c873e68394240d46b4b768f76ea229f67f7bbf9
KhaledSaud70 <Khaled.robusta@gmail.com>
2024-01-15 15:06:52 +0300

    init

--- dist_train.sh
+++ dist_train.sh
@@ -3,7 +3,7 @@
 MODEL=$1
 nGPUs=$2
 
-OUTPUT_DIR=/root/workspace/efficientformer_s0_100d
+OUTPUT_DIR=/root/workspace/scatformer_s0_200d
 
 # python -m torch.distributed.launch --nproc_per_node=$nGPUs --use_env /root/workspace/ScatFormer/main.py --model $MODEL \
 # --data-path /root/workspace/ImageNet100 \
@@ -16,8 +16,7 @@ python /root/workspace/ScatFormer/main.py --model $MODEL \
 --data-path /root/workspace/ImageNet100 \
 --output_dir $OUTPUT_DIR \
 --batch-size 256 \
---epochs 100 \
---resume /root/workspace/ScatFormer/efficientformer_s0_100d/checkpoint.pth \
+--epochs 200 \
 --distillation-type none \
 
 
